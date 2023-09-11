991d39ea1da0ebf45a874c5462bb9d678010e1f6
ABCdeveloper123 <sohaghasun4@gmail.com>
2023-09-11 10:38:44 +0600

    first commit

---
+++ ifelse.js
@@ -0,0 +1,14 @@
+var hero="khan";
+    if (hero == "ali"){
+        console.log("tomer ans vol");
+    } 
+    if (hero =="khan"){
+        console.log('tumi thik');
+    }
+    else {
+        console.log("hero");
+    }
+
+
+
+//
